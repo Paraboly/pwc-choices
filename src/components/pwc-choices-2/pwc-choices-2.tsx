5814b49e1d9ce55a2145f420e1a87d4890222685
starikcetin <cetinsamedtarik@gmail.com>
2019-12-21 18:35:49 +0300

    wip

--- src/components/pwc-choices-2/pwc-choices-2.tsx
+++ src/components/pwc-choices-2/pwc-choices-2.tsx
@@ -17,20 +17,47 @@ export class PwcChoices2Component {
     this.resolvedOptions = resolveJson(newValue);
   }
 
+  @State() isDropDownOpen: boolean = false;
+
   render() {
     return (
       <div>
         {this.generateInputBar()}
-        {this.generateDropdown()}
+        {this.isDropDownOpen && this.generateDropdown()}
       </div>
     );
   }
 
   generateInputBar() {
-    return <div>Input Bar</div>;
+    return (
+      <div class="input-bar" onClick={e => this.onInputBarClick(e)}>
+        Input Bar
+      </div>
+    );
+  }
+
+  onInputBarClick(e: MouseEvent): void {
+    this.isDropDownOpen = !this.isDropDownOpen;
   }
 
   generateDropdown() {
-    return <div>Dropdown</div>;
+    return (
+      <div class="dropdown">
+        <ul>
+          {this.resolvedOptions &&
+            this.resolvedOptions.map(option => this.generateOption(option))}
+        </ul>
+      </div>
+    );
+  }
+
+  generateOption(option: PwcChoices2.Option): any {
+    return <li onClick={e => this.onOptionClick(option, e)}>{option.label}</li>;
+  }
+
+  onOptionClick(option: PwcChoices2.Option, clickEvent: MouseEvent): void {
+    clickEvent.preventDefault();
+    console.log("option clicked");
+    console.log(option);
   }
 }
