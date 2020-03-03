0b2c9f1556e5beb154137218b283392a189fe29c
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2020-03-03 15:54:34 +0300

    disable fscache in builds
    ideally we want to use the cache, but when fscache gets corrupted stencil can't get anything done.

--- stencil.config.ts
+++ stencil.config.ts
@@ -16,5 +16,8 @@ export const config: Config = {
       serviceWorker: null // disable service workers
     }
   ],
-  plugins: [sass()]
+  plugins: [sass()],
+
+  // ideally we want to use the cache, but when fscache gets corrupted stencil can't get anything done.
+  enableCache: false
 };
